cd1134200b75378168b6fbf145392dc4d68ee973
alexfargo <kevin.a.fargo@gmail.com>
2025-03-18 09:41:29 -0600

    Open Swagger UI by default

--- src/main/java/org/msse640/triangle/TriangleMiddlewareApplication.java
+++ src/main/java/org/msse640/triangle/TriangleMiddlewareApplication.java
@@ -1,13 +1,43 @@
 package org.msse640.triangle;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class TriangleMiddlewareApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(TriangleMiddlewareApplication.class, args);
+        try {
+            openHomePage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void openHomePage() throws IOException {
+        String url = "http://localhost:8080";
+        Runtime rt = Runtime.getRuntime();
+        String os = System.getProperty("os.name").toLowerCase();
+
+        if (os.contains("win")) {
+            // Windows
+            rt.exec("rundll32 url.dll,FileProtocolHandler " + url);
+        } else if (os.contains("mac")) {
+            // macOS
+            rt.exec("open " + url);
+        } else if (os.contains("nix") || os.contains("nux")) {
+            // Unix or Linux
+            rt.exec("xdg-open " + url);
+        } else {
+            throw new UnsupportedOperationException("Unsupported operating system: " + os);
+        }
     }
 
 }
--- src/main/java/org/msse640/triangle/config/OpenApiConfig.java
+++ src/main/java/org/msse640/triangle/config/OpenApiConfig.java
@@ -5,8 +5,12 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
-public class OpenApiConfig {
+public class OpenApiConfig implements WebMvcConfigurer {
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -15,5 +19,10 @@ public class OpenApiConfig {
                         .title("Triangle Middleware API")
                         .version("1.0")
                         .description("API for determining the type of a triangle"));
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/swagger-ui.html");
     }
 }
